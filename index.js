25abc6d36ee8e9f841888b42b9e099b8d3569e87
janssea <alex.janssen@uberflip.com>
2020-06-26 20:35:14 -0400

    LUNETTA-100: Initial commit with lfo and clock divider functionality

---
+++ index.js
@@ -0,0 +1,69 @@
+const modular = {};
+const bpm = 120;
+const on = false;
+
+const runModular = () => {
+  initLfo();
+  initClockDivider();
+}
+
+const generateLfo = target => {
+    if (target.value === 0) {
+      target.value = 1;
+    } else {
+      target.value = 0;
+    }
+  }
+}
+
+const initLfo = () => {
+  modular.quadLfo = {
+    one: {
+      value: 0,
+      rate: 100,
+      intervalID: null,
+      outputTarget: null,
+    },
+    two: {
+      value: 0,
+      rate: 100,
+      intervalID: null,
+      outputTarget: null,
+    },
+    three: {
+      value: 0,
+      rate: 100,
+      intervalID: null,
+      outputTarget: null
+    },
+    four: {
+      value: 0,
+      rate: 100,
+      intervalID: null,
+      outputTarget: null
+    }
+  }
+
+  for (lfo in modular.quadLfo) {
+    lfo.intervalID = setInterval(
+      generateLfo(lfo), 
+      lfo.interval.rate
+    );
+  }
+}
+
+const initClockDivider = () => {
+  modular.clockDivider = {
+    clockInput: null,
+    dividers: {
+      two: null,
+      four: null,
+      eight: null,
+      sixteen: null,
+      thirtyTwo: null,
+      sixtyFour: null
+    } 
+  }
+}
+
+runModular();
